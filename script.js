cfb4ce9673b42672e9c65e9a9c79e47c019e9458
Fael <107376534+Faelzada@users.noreply.github.com>
2024-08-14 11:43:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.alert("Opa Jairão, bão?");
 var msg = window.document.getElementById('msg');
 var img = window.document.getElementById('foto');
 var data = new Date();
-var hora = 13 //data.getHours();
+var hora = data.getHours();
 
 if (hora >= 7 && hora < 12) {
     img.src = '3292e2078a6a7d8bce24d19361dd413b.jpg';
