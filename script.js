fe87f2682e0ef2dd928bbacb280e0a3d45fe29a2
Fael <107376534+Faelzada@users.noreply.github.com>
2024-08-14 11:34:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function carregar(){
-var msg - window.document.getElementById('msg') 
-  var foto -window. document.getElementById("imagem')
-  var data - new Date()
+var msg = window.document.getElementById('msg') 
+  var foto = window. document.getElementById("imagem')
+  var data = new Date()
   var hora = data.gethours()
   msg.innerHTL = `Agora sao $(hora) horas.'
   if (hora >=0 && hora < 12){  //Bom dia  img.src = '3292e2078a6a7d8bce24d19361dd413b.jpg'
